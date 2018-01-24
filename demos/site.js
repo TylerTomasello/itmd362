d7b8f889dde1c2b6651ca9b1f5c7e82407659b2d
TylerTomasello <ttomasel@hawk.iit.edu>
2018-01-24 15:53:41 -0600

    add console log of when the submit button is clicked

--- demos/site.js
+++ demos/site.js
@@ -2,3 +2,7 @@ $('#clicker').no('click', function(e) {
   $(this).html('I Have Been Clicked');
   e.preventDefault();
 });
+
+$('#contact-form').on('submit', function() {
+  console.log('The form was submitted.');
+});
